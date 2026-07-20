ae6b97b757010fc16e8203a8b34452673e2718fc
manish <manish.dataops@gmail.com>
2026-07-20 14:08:51 +0000

    updated

--- Sales_Analysis_.sql
+++ Sales_Analysis_.sql
@@ -1,5 +1,17 @@
+--DASHBOARD PROFIT GROWTH TREND ANALYSIS
+
 -- connect with database
 USE sales_analysis_db;
 --database table over vioew
 SELECT name FROM sys.tables;
+
+-- bussines problum statement
+SELECT 
+YEAR(order_date) AS year,
+SUM(profit) AS total_profit,
+LAG(SUM(profit),1) OVER (ORDER BY YEAR(order_date)) AS previous_year_profit,
+CAST((SUM(profit) * 100.0 / LAG(SUM(profit),1) OVER (ORDER BY YEAR(order_date))) -100  AS DECIMAL(5,2)) AS YoY_profit_growth
+FROM  sales_rawdata
+GROUP BY YEAR(order_date)
+
 
