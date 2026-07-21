c5c3969b17d195c152c14b7fea671a44b7ff701e
manish <manish.dataops@gmail.com>
2026-07-21 05:51:07 +0000

    latest updates

--- Sales_Analysis_.sql
+++ Sales_Analysis_.sql
@@ -6,12 +6,25 @@ USE sales_analysis_db;
 SELECT name FROM sys.tables;
 
 -- bussines problum statement
-SELECT 
+WITH CTE AS (
+    SELECT 
 YEAR(order_date) AS year,
 SUM(profit) AS total_profit,
 LAG(SUM(profit),1) OVER (ORDER BY YEAR(order_date)) AS previous_year_profit,
 CAST((SUM(profit) * 100.0 / LAG(SUM(profit),1) OVER (ORDER BY YEAR(order_date))) -100  AS DECIMAL(5,2)) AS YoY_profit_growth
 FROM  sales_rawdata
-GROUP BY YEAR(order_date)
+GROUP BY YEAR(order_date))
+SELECT
+MAX(CASE WHEN year = '2024' THEN previous_year_profit END) AS current_year,
+MAX(CASE WHEN year = '2023' THEN previous_year_profit END) AS previous_year,
+MAX(CASE WHEN year = '2024' THEN yoy_profit_growth END) AS current_year_gwt,
+MAX(CASE WHEN year = '2023' THEN yoy_profit_growth END) AS previous_year_gwt,
+MAX(CASE WHEN year = '2024' THEN yoy_profit_growth END) - MAX(CASE WHEN year = '2023' THEN yoy_profit_growth END) AS growth_rate_diff
+FROM CTE
+
+
+
+
+
 
 
