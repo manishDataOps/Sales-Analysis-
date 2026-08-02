6abce14097d946cae390ffb9980981dee667ad5c
manish <manish.dataops@gmail.com>
2026-08-02 13:09:18 +0000

    updated

--- Sales_Analysis_.sql
+++ Sales_Analysis_.sql
@@ -21,4 +21,24 @@ sales,
 cast((sales * 100.0 / pre_year_sales) - 100 as decimal(5,2)) as sales_growth_rate,
 profit,
 cast((profit * 100.0 / pre_year_profit - 100) as decimal(5,2)) as order_growth_rate
+from cte ;
+
+with cte as(
+    select 
+year(order_date) as year,
+case when discount <= 0.05 then '0-5' 
+    when discount <= 0.15 then '6-15' 
+    when discount <= 0.25 then '16-25' else 'above 25' end as discount_type,
+count(order_id) as order_rec,    
+sum(profit) as profit    
+from sales_rawdata
+group by year(order_date),
+    case when discount <= 0.05 then '0-5' 
+    when discount <= 0.15 then '6-15' 
+    when discount <= 0.25 then '16-25' else 'above 25' end 
+    )
+select
+*
 from cte
+where year in (2023,2024)
+order by year , discount_type
